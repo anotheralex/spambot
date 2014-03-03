a46e7fae7ca0ce7c84074918b400f1f1122a5f38
MontyWest <monty.p.west@gmail.com>
2014-03-03 20:08:09 +0000

    webpageimpl first draft

--- WebPageImpl.java
+++ WebPageImpl.java
@@ -1,5 +1,10 @@
 import java.net.*;
 import java.io.*;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
 * A possible interface representing a web page.
@@ -7,17 +12,28 @@ import java.io.*;
 */
 public class WebPageImpl implements WebPage {
 
+	private static final Pattern linkPattern = new Pattern.compile('\<a href\="()".*');
+	private static final Pattern emailPattern = new Pattern.compile('([\w\.\-_]+@[\w\.\-_]\.[A-Za-z])[ "]');
 	private String urlstr;
 	private URL url;
+	private Set<String> links = new HashSet<String>();
+	private Set<String> emails = new HashSet<String>();
 
-	public WebPageImpl(String urlstr) {
+	/**
+	*
+	*
+	* throws MalformedURLException
+	*/
+	public WebPageImpl(String urlstr) throws MalformedURLException{
 		this.urlstr = urlstr;
+		parseUrlSource();
 	}
 
 	/**
 	* Returns the URL that identifies this web page.
 	* @return the URL that identifies this web page.
 	*/
+	@Override
 	public String getUrl() {
 		return this.urlstr;
 	}
@@ -30,7 +46,10 @@ public class WebPageImpl implements WebPage {
 	*
 	* @return all the links on this webpage.
 	*/
-	Set<String> getLinks();
+	@Override
+	Set<String> getLinks() {
+		return Collections.unmodifiableSet(links);
+	}
 	
 	/**
 	* Returns all the emails on this webpage.
@@ -40,9 +59,48 @@ public class WebPageImpl implements WebPage {
 	*
 	* @return all the emails on this webpage.
 	*/
-	Set<String> getEmails();
+	@Override
+	Set<String> getEmails() {
+		return Collections.unmodifiableSet(emails);
+	}
 
 	// Also, implementing classes should override equals() to
 	// ensure that p1.equals(p2) returns true if and only if
 	// p1.getUrl().equals(p2.getUrl()) returns true
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(p2 instanceOf WebPageImpl)) {
+			return false
+		}
+		WebPage p2 = (WebPage) obj;
+		return this.getUrl().equals(p2.getUrl());
+	}
+
+	private void parseUrlSource() throws MalformedURLException {
+		
+		URL website = new URL(urlstr);
+		BufferedReader in = null;
+		try { 
+			in = new BufferedReader(
+				new InputStreamReader(
+					website.openStream()));
+
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				Matcher linkMatcher = linkPattern.matcher(inputLine);
+				while (linkMatcher.find()) {
+					links.add(linkMatcher.group());
+				}
+				Matcher emailMatcher = emailPattern.matcher(inputLine);
+				while (emailMatcher.find()) {
+					emails.add(emailMatcher.group());
+				}
+			}
+
+			in.close;
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
