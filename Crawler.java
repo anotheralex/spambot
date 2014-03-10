b25db113764434a286139e6c45d6429bf92b4bdf
MontyWest <monty.p.west@gmail.com>
2014-03-10 20:26:15 +0000

    Crawler added

---
+++ Crawler.java
@@ -0,0 +1,30 @@
+
+
+public class Crawler implements Runnable {
+
+	private static final int MAX_LINKS_FOLLOWED = 10;
+	private SpamBot spamBot;
+	private AtomicInteger linksFollowed = new AtomicInteger();
+
+	public Crawler(SpamBot spamBot) {
+		this.spamBot = spamBot;
+	}
+
+	public void run() {
+		if(linksFollowed.get() > MAX_LINKS_FOLLOWED) return;
+		String url = spamBot.getAndRemoveLink();
+		try {
+			WebPage wp = new WebPageImpl(url);
+			Set<String> fetchedLinks = wp.getLinks();
+			spamBot.addLinks(fetchedLinks);
+			Set<String> fetchedEmails = wp.getEmails();
+			spamBot.addEmails(fetchedEmails);		
+			spamBot.addSuccesses(url);
+			linksFollowed.incrementAndGet();
+		} catch (MalformedUrlException e) {
+			System.out.println("Malformed Url: " + url);
+		}
+		run();
+	}
+
+}
