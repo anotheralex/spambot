d49aee5eb51cc0c7edb92af8c7ec805a49c4ae6a
Alex Wollenschlaeger <alexw@panix.com>
2014-03-10 18:47:17 +0000

    first commit

---
+++ WebPageImplTest.java
@@ -0,0 +1,27 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.net.MalformedURLException;
+
+public class WebPageImplTest {
+
+	private WebPage wp;
+	private String strurl;
+
+	@Before
+	public void setup() {
+		strurl = "http://www.oracle.com/index.html";
+		try {
+			wp = new WebPageImpl(strurl);
+		} catch (MalformedURLException e) {
+			System.out.println("Malformed URL");
+		}
+	}
+
+	@Test
+	public void getUrlTest() {
+		String expected = strurl;
+		String actual = wp.getUrl();
+		assertEquals(expected, actual);
+	}
+
+}
